3a2e3838c41466151719ea282495d5db9dcc488b
Shadan0786 <Shadanashrafi82@gmail.com>
2026-04-14 11:46:20 +0530

    React App

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 const Home = () => {
-    const [Category,setCategory] = useState("ALL")
+    const [Category, setCategory] = useState("ALL")
     return (
         <div>
-            <Header/>
-            <ExploreMenu Category= {Category} setCategory={setCategory}/>
+            <Header />
+            <ExploreMenu Category={Category} setCategory={setCategory} />
+            <FoodDisplay Category={Category} />
         </div>
     )
 }
