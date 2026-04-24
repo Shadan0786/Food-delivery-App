cc3bfe62aab13c8eea5f4d5a696acc0d469d346f
Shadan0786 <Shadanashrafi82@gmail.com>
2026-04-24 12:16:46 +0530

    backend

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/FoodRoute.js"
+
+//config
+const app = express()
+const port = 4000
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//db connection
+connectDB();
+
+//api endpoints
+app.use("/api/food" , foodRouter)
+app.use("/images",express.static('uploads'))
+
+app.get("/",(req ,res)=>{
+    res.send("API working")
+})
+
+app.listen(port,()=>{
+    console.log(`Server Started on http://localhost:${port}`)
+})
+
+//mongodb+srv://shadan1:Shadan0786@cluster0.lpiba.mongodb.net/?appName=Cluster0
