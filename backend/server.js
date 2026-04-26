6d8a3a9d47d3535a922f4ea6acc5d4f13c9ce86a
Shadan0786 <Shadanashrafi82@gmail.com>
2026-04-27 00:15:29 +0530

    admin panel

--- backend/server.js
+++ backend/server.js
@@ -25,5 +25,3 @@ app.get("/",(req ,res)=>{
 app.listen(port,()=>{
     console.log(`Server Started on http://localhost:${port}`)
 })
-
-//mongodb+srv://shadan1:Shadan0786@cluster0.lpiba.mongodb.net/?appName=Cluster0
